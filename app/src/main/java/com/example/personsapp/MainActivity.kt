4c22116331fd1a3b181d06f7d8f61fa90dbcca2f
emirk <emir.karabey1@hotmail.com>
2022-05-06 17:02:08 +0300

    Page designs added

--- app/src/main/java/com/example/personsapp/MainActivity.kt
+++ app/src/main/java/com/example/personsapp/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.example.personsapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
+import com.example.personsapp.entity.Persons
 import com.example.personsapp.ui.theme.PersonsAppTheme
+import com.google.gson.Gson
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,22 +36,141 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MyNavigation()
                 }
             }
         }
     }
 }
+@Composable
+fun MyNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "homepage"){
+        composable("homepage"){
+            HomePage(navController = navController)
+        }
+        composable("person_registration_page"){
+            PersonRegistrationPage()
+        }
+        composable("person_detail_page/{person}", arguments = listOf(
+            navArgument("person"){type = NavType.StringType}
+        )){
+            val json = it.arguments?.getString("person")
+            val p = Gson().fromJson(json,Persons::class.java)
+            PersonDetailPage(person = p)
+        }
+    }
+}
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun HomePage(navController: NavController) {
+    val searchControl = remember { mutableStateOf(false) }
+    val tf = remember { mutableStateOf("") }
+    val personsList = remember { mutableStateListOf<Persons>() }
+
+    LaunchedEffect(key1 = true){
+        val k1 = Persons(1,"Ahmet","111111")
+        val k2 = Persons(2,"Zeynep","222222")
+        personsList.add(k1)
+        personsList.add(k2)
+    }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    if(searchControl.value){
+                        TextField(
+                            value = tf.value,
+                            onValueChange = {
+                                tf.value = it
+                                Log.e("Kişi arama",it)
+                            },
+                            label = { Text(text = "Search") },
+                            colors = TextFieldDefaults.textFieldColors(
+                                backgroundColor = Color.Transparent,
+                                focusedLabelColor = Color.White,
+                                focusedIndicatorColor = Color.White,
+                                unfocusedLabelColor = Color.White,
+                                unfocusedIndicatorColor = Color.White,
+                            )
+                        )
+                    }else{
+                        Text(text = "Persons")
+                    }
+                },
+                actions = {
+                    if(searchControl.value){
+                        IconButton(onClick = {
+                            searchControl.value = false
+                            tf.value = ""
+                        }) {
+                            Icon(painter = painterResource(id = R.drawable.ic_baseline_close_24),
+                                contentDescription = "",tint = Color.White)
+                        }
+                    }else{
+                        IconButton(onClick = {
+                            searchControl.value = true
+                        }) {
+                            Icon(painter = painterResource(id = R.drawable.ic_baseline_search_24),
+                                contentDescription = "",tint = Color.White)
+                        }
+                    }
+                }
+            )
+        },
+        content = {
+            LazyColumn{
+                items(
+                    count = personsList.count(),
+                    itemContent = {
+                        val person = personsList[it]
+                        Card(modifier = Modifier
+                            .padding(all = 5.dp)
+                            .fillMaxWidth()) {
+                            Row(modifier = Modifier.clickable {
+                                val personJson = Gson().toJson(person)
+                                navController.navigate("person_detail_page/${personJson}")
+                            }) {
+                                Row(
+                                    modifier = Modifier
+                                        .padding(all = 10.dp)
+                                        .fillMaxWidth(),
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    horizontalArrangement = Arrangement.SpaceBetween
+                                ) {
+                                    Text(text = "${person.person_name} - ${person.person_phone}")
+
+                                    IconButton(onClick = {
+                                        Log.e("Kişi Sil","${person.person_id}")
+                                    }) {
+                                        Icon(painter = painterResource(id = R.drawable.ic_baseline_delete_outline_24),
+                                            contentDescription = "",tint = Color.Gray)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                )
+            }
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { navController.navigate("person_registration_page") },
+                backgroundColor = colorResource(id = R.color.teal_200),
+                content = {
+                    Icon(painter = painterResource(id = R.drawable.ic_baseline_add_24),
+                        contentDescription = "",tint = Color.White)
+                }
+            )
+        }
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     PersonsAppTheme {
-        Greeting("Android")
+
     }
 }
---
+++ app/src/main/java/com/example/personsapp/PersonDetailPage.kt
@@ -0,0 +1,58 @@
+package com.example.personsapp
+
+import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.unit.dp
+import com.example.personsapp.entity.Persons
+
+@Composable
+fun PersonDetailPage(person:Persons) {
+    val tfPersonName = remember{ mutableStateOf("") }
+    val tfPersonPhone = remember{ mutableStateOf("") }
+    val localFocusManager = LocalFocusManager.current//tf deki focusu kaldırıyor
+    //launc effect sayfa açıldığı anda çalışır
+    LaunchedEffect(key1 = true){
+        tfPersonName.value = person.person_name
+        tfPersonPhone.value = person.person_phone
+    }
+    
+    Scaffold(
+        topBar = {
+            TopAppBar(title ={ Text(text = "Person Detail") } )
+        },
+        content = {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.SpaceEvenly,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                TextField(value = tfPersonName.value,
+                    onValueChange = {tfPersonName.value=it},
+                    label = { Text(text = "Person Name") })
+                TextField(value = tfPersonPhone.value,
+                    onValueChange = {tfPersonPhone.value=it},
+                    label = { Text(text = "Person Phone") })
+                Button(onClick = {
+                    val person_name = tfPersonName.value
+                    val person_phone =tfPersonPhone.value
+                    Log.e("kişi güncelle","${person.person_id} - $person_name - $person_phone")
+                    localFocusManager.clearFocus()
+                }, modifier = Modifier.size(250.dp,50.dp)) {
+                    Text(text = "UPDATE")
+                }
+            }
+        }
+
+    )
+}
